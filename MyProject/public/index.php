3af6000c512df4f07b82baa2abfec9fdd1be4267
peter_wang_invigor <peter.wang@invigorgroup.com>
2019-12-06 16:02:49 +1100

    make master branch runablerunnable?

--- MyProject/public/index.php
+++ MyProject/public/index.php
@@ -1,17 +1,16 @@
 <?php
-
 /**
  * X enter file
  * @author peter wang <stone256@hotmail.com>
  * @copyright GPL
- * @version 2.1
+ * @version 2.1.0.1
  *
  */
 //fix issue $_SERVER['REQUEST_TIME.. with microtime
 define('_X_START_TIME', microtime(1));
 //my root
 define('_X_INDEX', __DIR__);
-
+error_reporting(E_ALL);
 //load framework
 require_once(__DIR__ . '/../_system/app.php');
 
