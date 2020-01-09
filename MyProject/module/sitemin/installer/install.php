855ad83e47f953d478b1ebfad4c4ae31781e26c4
Your Name <you@example.com>
2020-01-10 10:38:00 +1100

    installer update-28

--- MyProject/module/sitemin/installer/install.php
+++ MyProject/module/sitemin/installer/install.php
@@ -86,10 +86,10 @@ class sitemin_installer_install{
                 //set the database config (_config('DATABASE,a'))
                 $con = file_get_contents(_X_INSTALL_FILE5);
                 //$con = preg_replace('/\s*\,/ims', "'host'=>'{$q['host']}'" , $con);
-                $con = preg_replace('/\s*\'host\'\s*\=\>\s*\'localhost\'\s*\,/ims', "'host'=>'{$q['host']}'," , $con);
-                $con = preg_replace('/\s*\'database\'\s*\=\>\s*\'mydatabase\'\s*\,/ims', "'database'=>'{$q['database']}'," , $con);
-                $con = preg_replace('/\s*\'user\'\s*\=\>\s*\'myusername\'\s*\,/ims', "'user'=>'{$q['user']}'," , $con);
-                $con = preg_replace('/\s*\'password\'\s*\=\>\s*\'mypassword\'\s*\,/ims', "'password'=>'{$q['password']}'," , $con);
+                $con = preg_replace('/\s*\'host\'\s*\=\>\s*\'localhost\'\s*\,/ims', "\n'host'=>'{$q['host']}'," , $con);
+                $con = preg_replace('/\s*\'database\'\s*\=\>\s*\'mydatabase\'\s*\,/ims', "\n'database'=>'{$q['database']}'," , $con);
+                $con = preg_replace('/\s*\'user\'\s*\=\>\s*\'myusername\'\s*\,/ims', "\n'user'=>'{$q['user']}'," , $con);
+                $con = preg_replace('/\s*\'password\'\s*\=\>\s*\'mypassword\'\s*\,/ims', "\n'password'=>'{$q['password']}',\n" , $con);
                 file_put_contents( _X_INSTALL_FILE5, $con);
                 rename(_X_INSTALL_FILE5, str_replace('.sample', '', _X_INSTALL_FILE5));
                 return;
