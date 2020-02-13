395bbafc515e425bde506923edcfb793b9383471
Your Name <you@example.com>
2020-02-13 11:27:01 +1100

    add back blink class to css

--- MyProject/public/package/common/common.js
+++ MyProject/public/package/common/common.js
@@ -4,20 +4,24 @@ var common ={
 
 	mask:{
 		id:'__mask_for_loading',
+		on:false,
 		up:function(con){
-			common.mask.down();
+			if(common.mask.on) return;
+			common.mask.on = true;
+
 			var w=$(window).width();
 			var h=$(window).height();
-            con = con || '';
-			var html = '<div id="'+common.mask.id+'" style="z-index:50000;top:0;left:0;width:'+w+'px;height:'+h+'px;position:fixed; opacity:0.7;background:#000;text-align:center" > <div style="margin-top:'+(h/2)+'px;margin-left:'+(w/2-16)+'px" class="loader"></div><b style="line-height:4px;opacity:1;color:#ffff00">'+con+'</b></div>' ;
-
-//				var html = '<div id="'+common.mask.id+'" style="z-index:50000;top:0;left:0;width:'+w+'px;height:'+h+'px;position:fixed; opacity:0.6;background:#eee url(/media/image/ajax-loader.gif) 50% 50% no-repeat;" ></div>' ;
+            		con = con || '';
+			var loader = '<div style="margin-top:'+(h/2)+'px;margin-left:'+(w/2-16)+'px" class="'+(con ? '' : 'loader')+'"></div>'
+			var html = '<div id="'+common.mask.id+'" style="z-index:50000;top:0;left:0;width:'+w+'px;height:'+h+'px;position:fixed; opacity:0.7;background:#000;text-align:center" >'+loader+' <b class="blinking" style="line-height:4px;opacity:1;color:#ffff00">'+con+'</b></div>' ;
+			//var html = '<div id="'+common.mask.id+'" style="z-index:50000;top:0;left:0;width:'+w+'px;height:'+h+'px;position:fixed; opacity:0.6;background:#eee url(/media/image/ajax-loader.gif) 50% 50% no-repeat;" ></div>' ;
 
 			$('body').append(html)
 			$('#'+common.mask.id).width();
 			return true;
 		},
 		down:function(){
+			common.mask.on = false;
 			$('#'+common.mask.id).remove();
 		}
 
@@ -62,8 +66,20 @@ var common ={
 			v.stop(a.currentTime+duration*0.001)
 		},
 	},
-
-
+	random : (min, max)=> {
+	  return Math.floor(Math.random() * (max - min)) + min;
+  	},
+	ND:(start, end)=>{
+		start = start || 0
+		end = end || 1
+		let u = 0, v = 0;
+		while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+		while(v === 0) v = Math.random();
+		let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+		num = num / 10.0 + 0.5; // Translate to 0 -> 1
+		if (num > 1 || num < 0) return common.ND(start, end); // resample between 0 and 1
+		return parseInt(start+num*end);
+	}
 
 }
 
